3901cff3cdd5cc34258a40cbedb2241b77ac4b4f
manru-mj <manrumc@gmail.com>
2025-06-05 19:15:37 -0600

    directory not needed

--- semana_6/Semana 6 - Funciones - Ejercicio 4.py
+++
@@ -1,9 +0,0 @@
-def invert_string(parameter):
-    new_string = ""
-    for i in range(len(parameter),0,-1):
-        new_string += parameter[i-1]
-    return new_string    
-
-
-my_string = input("Type a String: ")
-print(f"The inverted string is: {invert_string(my_string)}")
