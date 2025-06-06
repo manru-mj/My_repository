60c953f87e3bd0709e6369c76aae6fd2bc606bde
manru-mj <manrumc@gmail.com>
2025-06-05 19:09:12 -0600

    Delete semana_3 directory

--- semana_3/Ejercicio 1.py
+++
@@ -1,3 +0,0 @@
-print("Semana 3")
-print("Ejercicio de Introducción a Python #1")
-print("Use la función print() para mostrar distintos textos en pantalla. ")
--- semana_3/Ejercicio 2.py
+++
@@ -1,9 +0,0 @@
-num_uno = 20
-num_dos = 2
-
-print("Los números son " + str(num_uno) + " y " + str(num_dos))
-print("La Suma es = " + str(num_uno+num_dos))
-print("La Resta es = " + str(num_uno-num_dos))
-print("La Multiplicación es = " + str(num_uno*num_dos))
-print("La División es = " + str(num_uno/num_dos))
-print("El Módulo es = " + str(num_uno%num_dos))
